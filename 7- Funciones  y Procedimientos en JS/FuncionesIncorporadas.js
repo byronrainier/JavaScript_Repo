1cdbdbeac14e2edd9545bc7fad3688809439ee35
Byron Henriquez <56271561+byronrainier@users.noreply.github.com>
2024-01-15 11:40:40 -0400

    15012024

---
+++ 7- Funciones  y Procedimientos en JS/FuncionesIncorporadas.js
@@ -0,0 +1,26 @@
+// Funciones incorporadas en JavaScript
+
+// De cadena
+
+let miCadena = 'BarryZito';
+let miCadena2 = 'SanFrancisco Giants';
+
+console.log(miCadena.length);
+console.log(miCadena.substring(0,4));
+console.log(miCadena + ' ' + miCadena2);
+
+// De tipo matematico
+
+// Obtener valor absoluto de un numero
+let num1 = -75, valorAbsoluto = Math.abs(num1);
+console.log(valorAbsoluto);
+// Redondeo de un numero
+let num2 = 23.1, valorRedondeado = Math.round(num2);
+console.log(valorRedondeado);
+// Trunqueo de un numero (Eliminar la parte flotante y/o decimal)
+let num3 = 23.75, valorTruncado = Math.trunc(num3);
+console.log(valorTruncado);
+
+
+
+// De tipo numerico
